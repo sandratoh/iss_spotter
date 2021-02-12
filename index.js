0365f3ffc8156fd6d96c31da9a31275c40d71e22
Sandra To <sandra.to.e@gmail.com>
2021-02-12 10:45:13 +0000

    can print date and time as one string in console.log, manually adding in gmt-0700

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const { nextISSTimesForMyLocation } = require('./iss');
 const printPassTimes = passTimes => {
   for (let pass of passTimes) {
     
-    // let time = new Date(pass.risetime).toLocaleTimeString('us-en', { hour12: false});
     let datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
     
-    const dateOptions = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric', timeZone: 'Canada/Pacific' };
-    const pstDate = datetime.toLocaleDateString('en-CA', dateOptions);
+    const dateTimeOptions = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour12:false,
+      timeZone: 'Canada/Pacific',
+      timeZoneName: 'long'
+    };
     
-    const timeOptions = { hour12:false, timeZone: 'Canada/Pacific' };
-    const pstTime = datetime.toLocaleTimeString('en-CA', timeOptions);
-    
-    // let pstDateTime = datetime.toLocaleString('en-CA', options);
+    let pstDateTime = datetime.toLocaleString('en-CA', dateTimeOptions);
     
     let duration = pass.duration;
     
-    console.log(`Next pass at ${pstDate} ${pstTime} GMT-0700 (Pacific Daylight Time) for ${duration} seconds!`);
+    console.log(`Next pass at ${pstDateTime} GMT-0700 for ${duration} seconds!`);
   }
 };
 
