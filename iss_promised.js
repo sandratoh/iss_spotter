f07619cd5bcfefadfbc57da4f1dfd3fa976ad59f
Sandra To <sandra.to.e@gmail.com>
2021-02-12 21:35:38 +0000

    Use promises to fetch ip, coords, and fly over times

---
+++ iss_promised.js
@@ -0,0 +1,24 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => request('https://api.ipify.org/?format=json');
+
+/*
+ * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = body => {
+  let ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = (body) => {
+  let coords = {};
+  coords.latitude = JSON.parse(body).latitude;
+  coords.longitude = JSON.parse(body).longitude;
+  const lat = coords.latitude;
+  const lon = coords.longitude;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}`);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
